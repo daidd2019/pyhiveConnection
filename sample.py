a184c61e63c00b0754b5e22f06dd690903ec2a82
liuhb <super_sponge@163.com>
2020-01-14 14:55:43 +0800

    add exit code

--- sample.py
+++ sample.py
@@ -1,5 +1,8 @@
+import sys
 from pyhiveConnection import hiveConnector
 conn = hiveConnector.KRBZKConnect('default')
+if conn == 0:
+    sys.exit(1)
 cursor=conn.cursor()
 cursor.execute("show databases")
 for result in cursor.fetchall():
